0a54ba590f95c898bb5a0fdc2daaef3897782302
Felipe Bezerra <felipebnews@gmail.com>
2022-11-23 12:22:41 -0300

    feat: Car-router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
+const CarRouter = require('./CarRouter');
 
 module.exports = (server) => {
   server.use((req, res, next) => {
+    CarRouter(server, new Router());
     next();
   });
 };
